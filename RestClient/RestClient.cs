f3d14be66ce912874411a1d23946cc3bcafc881b
OptisMike <Kokorin.huawei@gmail.com>
2023-04-05 15:35:10 +0300

    add comment

--- RestClient/RestClient.cs
+++ RestClient/RestClient.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Net;
 
 namespace RestClient
 {
@@ -40,6 +35,8 @@ namespace RestClient
                 {
                     throw new ApplicationException("error code: " +  response.StatusCode);
                 }
+                
+                //Process the response stream...(cloud be JSON, XML or HTML)
 
                 using(Stream  responseStream = response.GetResponseStream())
                 {
@@ -51,7 +48,11 @@ namespace RestClient
                         }
                     }
                 }
+
+                //End of using ResponseStream
             }
+
+            //End of using response
 
             return strResponseValue;
         }
